1682a324e4f169824e831b02cdbb702c932925db
maksim <maks.sidenko.02@gmail.com>
2023-09-13 00:36:55 +0300

    13_09_23_first_commit

---
+++ myProjects/trainProject1/index.php
@@ -0,0 +1,8 @@
+<form action="" method="$_POST">
+    <input type="text" name="login" placeholder="Type login" required>
+    <input type="password" name="pass" placeholder="Type pass" required>
+    <input type="email" name="email" placeholder="Type email" required>
+    <input type="tel" name="tel" placeholder="Type phone">
+    <input type="submit" value="send">
+
+</form>
